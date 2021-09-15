855bc365fa1b42cc1b0ab2bd1e7c245e1e1097a1
Christopher Read <Christopherread22@outlook.com>
2021-09-15 17:18:19 -0400

    Axios

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import axios from 'axios'
 import './App.css';
 
 function App() {
+    const [image, setImage] = useState('')
+
+    const handleChange = () => {
+        axios.get('https://api.generated.photos/api/v1/faces?api_key=1VnOVaWgoNbg7LMJfMbTsg').then(res => {
+            console.log(res.data)
+        }).catch(err => {
+            console.log(err.message)
+        })
+    }
+
     return (
         <div className="App">
             <h1>Hello</h1>
